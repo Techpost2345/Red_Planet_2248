ba0e84b5b7aee838108081a393e87014f3075934
Techpost <techpost12345@gmail.com>
2026-06-01 18:58:17 -0500

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -16,7 +16,7 @@
     // Add your custom code here.
   };
 
-  var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
+  var TITLE = "Red Planet 2248" + '_' + "Autumn Chen" + '_' + "Mod by Techpost";
 
   // the url is a link to game.json
   // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
